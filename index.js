b4ac3589cdfcc01ba61de25bd97b9493a1d5565a
Mustafa Fahmy <mustafa.fahmyy93@gmail.com>
2024-01-04 06:24:24 +0200

    update port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import { geojsonData } from "./tideData.js";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(compression());
